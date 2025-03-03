344c87bb5aac151b17199c32410d62497675cbf3
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2025-03-03 14:45:54 -0700

    update: main to create txt file

--- simulacion/MonedaYDado/main.cpp
+++ simulacion/MonedaYDado/main.cpp
@@ -1,36 +1,57 @@
 #include "Manejador.h"
 #include <iostream>
+#include <fstream>
 
-void printMenu() {
+void printMenu(int& seleccion) {
   system("clear");
   std::cout << "###### MENU PRINCIPAL ######" << std::endl;
   std::cout << "1. Lanzar monedas" << std::endl;
   std::cout << "2. Lanzar dados" << std::endl;
   std::cout << "3. Salir" << std::endl;
+
+  std::cout << "\nSeleccione una opcion: ";
+  std::cin >> seleccion;
 }
 
-int main() {
+void esperar() {
+  std::cout << "\nPresione Enter para continuar...";
+  std::cin.ignore();
+  std::cin.get();
+}
+
+void getNombreArchivo(std::string& nombreArchivo) {
+  std::cout << "\nIntroduzca el nombre del archivo: ";
+  std::cin >> nombreArchivo;
+}
+
+void almacenaResultados(const std::string& nombreArchivo, const std::string& resultados) {
+  std::string archivoExtension = nombreArchivo + ".txt";
+  std::ofstream archivo;
+  archivo.open(archivoExtension);
+  archivo << resultados;
+  archivo.close();
+}
+
+int main() { 
   Manejador manejador;
+  std::string nombreArchivo;
 
   int seleccion = 0;
 
   while (seleccion != 3) {
-    printMenu();
-    std::cout << "\nSeleccione una opcion: ";
-    std::cin >> seleccion;
+    printMenu(seleccion);
+
     if (seleccion == 1) {
+      getNombreArchivo(nombreArchivo);
       system("clear");
-      manejador.LanzarMonedas();
-      std::cout << "\nPresione Enter para continuar...";
-      std::cin.ignore();
-      std::cin.get();
+      almacenaResultados(nombreArchivo, manejador.LanzarMonedas());
+      esperar();
 
     } else if (seleccion == 2) {
+      getNombreArchivo(nombreArchivo);
       system("clear");
-      manejador.LanzarDados();
-      std::cout << "\nPresione Enter para continuar...";
-      std::cin.ignore();
-      std::cin.get();
+      almacenaResultados(nombreArchivo, manejador.LanzarDados());
+      esperar();
     }
   }
 
